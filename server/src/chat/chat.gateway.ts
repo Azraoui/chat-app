cc630b25b5b1e55ad9a45ad2d0f83e467f492c67
Azraoui <ael-azra@student.1337.ma>
2023-01-13 22:12:58 +0100

    implement ChatGateway

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -1,21 +1,46 @@
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer
   } from '@nestjs/websockets'
 
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway({
+  cors: {
+    origin: '127.0.0.1:3000',
+  }
+})
+
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
 
   @WebSocketServer() server: Server;
 
-  @SubscribeMessage('new_message')
+  @SubscribeMessage('send_message')
   listenForMessage(@MessageBody() msgContent) {
     
     console.log(msgContent);
     this.server.sockets.emit('recieve_message', msgContent);
+  }
+
+  // OnGatewayInit
+  afterInit(server: Server) {
+    console.log(`server = ${server}`);
+  }
+  
+  // OnGatewayConnection
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Connected ${client.id}`);
+    
+  }
+  
+  // OnGatewayDisconnect
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected: ${client.id}`);
+    
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '127.0.0.1:300',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT']
+  })
   await app.listen(5000);
 }
+
 bootstrap();
